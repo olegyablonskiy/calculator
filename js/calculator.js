0ba9398e5e8541aaae2d5f17ce2c17119b284c8d
oleg yablonskiy <olegyablonskiy1993@gmail.com>
2018-04-03 13:36:22 +0000

    in

--- js/calculator.js
+++ js/calculator.js
@@ -1,43 +1,55 @@
-document.querySelectorAll('.buttons button')
-    .forEach(el => addEventListner('click', caclcBtn);
-
-const calc = $('.calculator');
-const calcDisplay = calc.find('.calc-display');
-//const calcKeys = calc.find('.calculator__key');
-const calcButton = calc.find('.calclBtn');
-const calcClear = calc.find('.calc-clear');
-const calcEqual = calc.find('.calc-equal');
-const calcMult = calc.find('.calc-mult');
-const calcDivide = calc.find('.calc-divide');
-
-calcBtn.each(function () {
-    let current = $(this).attr('value');
-    $(this).text(current);
-});
-
-calcButton.on('click', function () {
-        calcDisplay.val( calcDisplay.val() + $(this).attr('value') );
-
-function basic(operation, value1, value2) {
-    return eval(value1 + operation + value2);
+function c(val) {
+    document.getElementById("display").value = val;
+}
+function mat(val) {
+    document.getElementById("display").value += val;
 }
 
 
 
-function basicCalc(operation, value1, value2) {
-    switch (operation) {
-        case '+':
-            return value1 + value2;
-        case '-':
-            return value1 - value2;
-        case '*':
-            return value1 * value2;
-        case '/':
-            return value1 / value2;
-        default:
-            return 0;
-    }
-}
+
+
+
+// document.querySelectorAll('.buttons button')
+//     .forEach(el => addEventListner('click', caclcBtn);
+
+// const calc = $('.calculator');
+// const calcDisplay = calc.find('.calc-display');
+// //const calcKeys = calc.find('.calculator__key');
+// const calcButton = calc.find('.calclBtn');
+// const calcClear = calc.find('.calc-clear');
+// const calcEqual = calc.find('.calc-equal');
+// const calcMult = calc.find('.calc-mult');
+// const calcDivide = calc.find('.calc-divide');
+
+// calcBtn.each(function () {
+//     let current = $(this).attr('value');
+//     $(this).text(current);
+// });
+
+// calcButton.on('click', function () {
+//         calcDisplay.val( calcDisplay.val() + $(this).attr('value') );
+
+// function basic(operation, value1, value2) {
+//     return eval(value1 + operation + value2);
+// }
+
+
+
+// function basicCalc(operation, value1, value2) {
+//     switch (operation) {
+//         case '+':
+//             return value1 + value2;
+//         case '-':
+//             return value1 - value2;
+//         case '*':
+//             return value1 * value2;
+//         case '/':
+//             return value1 / value2;
+//         default:
+//             return 0;
+//     }
+// }
 
 
 
